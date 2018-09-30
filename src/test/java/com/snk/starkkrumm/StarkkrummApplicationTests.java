723b8e868f7197401b7c0083f6d241baee638091
Edvard Adam Eros <edvard_adam_eros@epam.com>
2018-09-30 21:41:01 +0200

    init repo

---
+++ src/test/java/com/snk/starkkrumm/StarkkrummApplicationTests.java
@@ -0,0 +1,16 @@
+package com.snk.starkkrumm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StarkkrummApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
