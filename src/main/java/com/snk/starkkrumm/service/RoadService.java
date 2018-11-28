032387dd68e6913b6b0cdea0cac7a379a0a8fe2c
Edvard Adam Eros <edvard_adam_eros@epam.com>
2018-11-28 21:04:23 +0100

    Row/Cell indentations complete

--- src/main/java/com/snk/starkkrumm/service/RoadService.java
+++ src/main/java/com/snk/starkkrumm/service/RoadService.java
@@ -4,12 +4,9 @@ import com.snk.starkkrumm.model.Road;
 import com.snk.starkkrumm.repository.RoadRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.BorderStyle;
-import org.apache.poi.ss.usermodel.CellStyle;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -17,14 +14,33 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static java.time.format.DateTimeFormatter.ofPattern;
+import static java.util.Arrays.asList;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class RoadService {
+
+    private static final int CELL_NUM_0 = 0;
+    private static final int CELL_NUM_1 = 1;
+    private static final int CELL_NUM_2 = 2;
+    private static final int CELL_NUM_3 = 3;
+    private static final int CELL_NUM_5 = 5;
+    private static final int CELL_NUM_10 = 10;
+    private static final int ROW_INDEX_3 = 3;
+    private static final String INPUT_XLS_FILE = "FAZ.xls";
+    private static final String YYYY_MM = "yyyy-MM";
+    private static final String DD_HH_MM = "dd.HH.mm";
+    private static final String SNK1 = ".SNK";
+    private static final String SNK2 = "-SNK-";
+    private static final String XLS_EXTENSION = ".xls";
+    private static final String NR_CIRCULATIE_SM = "NR. CIRCULAȚIE: SM.";
 
     private final RoadRepository roadRepository;
 
@@ -35,19 +51,15 @@ public class RoadService {
     public void populateExcelFiles(LocalDateTime departure) {
         List<Road> roads = roadRepository.findByDeparture(departure);
         Map<Integer, List<Road>> integerListMap = categorizeRoadsByCarNumber(roads);
-        try {
-            createExcels(integerListMap);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        createExcels(integerListMap);
     }
 
-    public Map<Integer, List<Road>> categorizeRoadsByCarNumber(List<Road> roads) {
+    private Map<Integer, List<Road>> categorizeRoadsByCarNumber(List<Road> roads) {
         Map<Integer, List<Road>> categorizedRoads = new HashMap<>();
         for (Road road : roads) {
             int carNumber = road.getCarNumber();
             if (!categorizedRoads.containsKey(carNumber)) {
-                categorizedRoads.put(carNumber, new ArrayList<>(Arrays.asList(road)));
+                categorizedRoads.put(carNumber, new ArrayList<>(asList(road)));
             } else {
                 List<Road> roadsByCarNumber = categorizedRoads.get(carNumber);
                 roadsByCarNumber.add(road);
@@ -57,55 +69,33 @@ public class RoadService {
         return categorizedRoads;
     }
 
-    public void createExcels(Map<Integer, List<Road>> map) throws IOException {
+    private void createExcels(Map<Integer, List<Road>> map) {
         for (Map.Entry<Integer, List<Road>> entry : map.entrySet()) {
-            FileInputStream in = new FileInputStream("FAZ.xls");
-            HSSFWorkbook workbook = new HSSFWorkbook(in);
-            CellStyle style = workbook.createCellStyle();
-            style.setBorderBottom(BorderStyle.THIN);
-            style.setBorderLeft(BorderStyle.THIN);
-            style.setBorderRight(BorderStyle.THIN);
-            style.setBorderTop(BorderStyle.THIN);
-            HSSFSheet sheet = workbook.getSheetAt(0);
-            List<Road> roads = entry.getValue();
-            int i = 11;
-            for (Road road : roads) {
-                HSSFRow row = sheet.createRow(i++);
-                row.setRowStyle(style);
-                HSSFCell cell = row.createCell(1);
-                cell.setCellValue(road.getRoadNumber());
-                cell.setCellStyle(style);
-                cell = row.createCell(2);
-                cell.setCellValue(road.getCarNumber());
-                cell.setCellStyle(style);
-                cell = row.createCell(3);
-                cell.setCellValue(road.getDriverName());
-                cell.setCellStyle(style);
-                cell = row.createCell(4);
-                cell.setCellValue(road.getDeparture().toString());
-                cell.setCellStyle(style);
-                cell = row.createCell(5);
-                cell.setCellValue(road.getArrival().toString());
-                cell.setCellStyle(style);
-                cell = row.createCell(6);
-                cell.setCellValue(road.getDistance());
-                cell.setCellStyle(style);
-                cell = row.createCell(7);
-                cell.setCellValue(road.getConsumption());
-                cell.setCellStyle(style);
-            }
-            FileOutputStream out = null;
             try {
-                out = new FileOutputStream(new File(roads.get(0).getDeparture().format(ofPattern("yyyy-MM"))
-                        + "-SNK-"
-                        + entry.getKey().toString()
-                        + ".xls"));
+                FileInputStream in = new FileInputStream(INPUT_XLS_FILE);
+                HSSFWorkbook workbook = new HSSFWorkbook(in);
+                HSSFSheet sheet = workbook.getSheetAt(0);
+                sheet.getRow(ROW_INDEX_3).getCell(11).setCellValue(NR_CIRCULATIE_SM + entry.getKey() + SNK1);
+                List<Road> roads = entry.getValue();
+                int i = 11;
+                for (Road road : roads) {
+                    HSSFRow row = sheet.getRow(i++);
+                    row.getCell(CELL_NUM_0).setCellValue(road.getDeparture().format(ofPattern(DD_HH_MM)));
+                    row.getCell(CELL_NUM_1).setCellValue(road.getArrival().format(ofPattern(DD_HH_MM)));
+                    row.getCell(CELL_NUM_2).setCellValue(road.getRoadNumber());
+                    row.getCell(CELL_NUM_3).setCellValue(road.getDriverName());
+                    row.getCell(CELL_NUM_5).setCellValue(road.getDistance());
+                    row.getCell(CELL_NUM_10).setCellValue(road.getConsumption());//08
+                }
+                FileOutputStream out = new FileOutputStream(new File(roads.get(0).getDeparture().format(ofPattern(YYYY_MM))
+                        + SNK2 + entry.getKey().toString() + XLS_EXTENSION));
                 workbook.write(out);
                 in.close();
                 out.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
         }
     }
 }
