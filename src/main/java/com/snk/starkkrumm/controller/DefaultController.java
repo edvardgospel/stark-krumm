74c2ff29e4f37894ea644786eea745087d4829ba
Edvard Adam Eros <edvard_adam_eros@epam.com>
2018-12-16 16:47:31 +0100

    Added mapping for admin login based on role

--- src/main/java/com/snk/starkkrumm/controller/DefaultController.java
+++ src/main/java/com/snk/starkkrumm/controller/DefaultController.java
@@ -1,7 +1,10 @@
 package com.snk.starkkrumm.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class DefaultController {
@@ -24,6 +27,17 @@ public class DefaultController {
     @GetMapping("/login")
     public String login() {
         return "/login";
+    }
+
+    @PostMapping("/login")
+    public String loginPost() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String role = authentication.getAuthorities().toString();
+        if (role.equals("ADMIN")) {
+            return "/admin";
+        } else {
+            return "/login";
+        }
     }
 
     @GetMapping("/403")
