73b08f37a8741258e05013f90f9d8c31064d83f8
Edvard Eros <edvard_adam_eros@epam.com>
2018-12-20 18:59:19 +0100

    Spring Security works correctly - will need refactor !!

--- src/main/java/com/snk/starkkrumm/config/SecurityConfig.java
+++ src/main/java/com/snk/starkkrumm/config/SecurityConfig.java
@@ -6,46 +6,35 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.web.access.AccessDeniedHandler;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private AccessDeniedHandler accessDeniedHandler;
-
-    // roles admin allow to access /admin/**
-    // roles user allow to access /user/**
-    // custom 403 access denied handler
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "/login").permitAll()
+                .antMatchers("/").permitAll()
                 .antMatchers("/admin/**").hasAnyRole("ADMIN")
                 .antMatchers("/user/**").hasAnyRole("USER")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/login")
+                .loginPage("/")
                 .permitAll()
                 .and()
                 .logout()
-                .permitAll()
-                .and()
-                .exceptionHandling().accessDeniedHandler(accessDeniedHandler);
+                .permitAll();
     }
 
-    // create two users, admin and user
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 
         auth.inMemoryAuthentication()
-                .withUser("starkkrumm_user").password("{noop}password").roles("USER")
+                .withUser("user").password("{noop}password").roles("USER")
                 .and()
-                .withUser("starkkrumm_admin").password("{noop}password").roles("ADMIN");
+                .withUser("admin").password("{noop}password").roles("ADMIN");
     }
 }
--- src/main/java/com/snk/starkkrumm/controller/DefaultController.java
+++ src/main/java/com/snk/starkkrumm/controller/DefaultController.java
@@ -4,45 +4,23 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class DefaultController {
-
     @GetMapping("/")
-    public String login1() {
-        return "/login";
-    }
-
-    @GetMapping("/admin")
-    public String admin() {
-        return "/admin";
-    }
-
-    @GetMapping("/user")
-    public String user() {
-        return "/user";
-    }
-
-    @GetMapping("/login")
-    public String login() {
-        return "/login";
-    }
-
-    @PostMapping("/login")
-    public String loginPost() {
+    public String home() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String role = authentication.getAuthorities().toString();
-        if (role.equals("ADMIN")) {
-            return "/admin";
-        } else {
-            return "/login";
+        log.info("ROLE_home: {}", role);
+        if (role.equals("[ROLE_ADMIN]")) {
+            return "admin";
+        } else if (role.equals("[ROLE_USER]")) {
+            return "user";
         }
+        return "login";
     }
-
-    @GetMapping("/403")
-    public String error403() {
-        return "/403";
-    }
-
 }
