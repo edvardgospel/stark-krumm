2cfd62fe858543ace31742f6ac236ba09797cd2e
Edvard Adam Eros <edvard_adam_eros@epam.com>
2019-01-27 23:26:10 +0100

    refactored js file (v2)

--- src/main/resources/static/snk.js
+++ src/main/resources/static/snk.js
@@ -1,17 +1,40 @@
-function monthChanged(month) {
-     $.get("/starkkrumm/month?month="+month, function (months) {
-             $("#commentField").html(months);
-     })
+function saveRoad() {
+    $.post("/starkkrumm/sendV2",
+    {"roadNumber": $("#roadNumber").val(),
+    "carNumber": $("#carNumber").val(),
+    "driverName": $("#driverName").val(),
+    "departure": $("#departure").val(),
+    "arrival": $("#arrival").val(),
+    "date": $("#date").val(),
+    "distanceBig": $("#distanceBig").val(),
+    "distanceSmall": $("#distanceSmall").val(),
+    "consumption1": $("#consumption1").val(),
+    "consumption2": $("#consumption2").val(),
+    "consumption3": $("#consumption3").val()})
 }
 
-$('#contact').submit(function (roadNumber,carNumber,driverName,departure,arrival,distance,departure) {
- $.post("/starkkrumm/send"+
-             "?roadNumber="+roadNumber+
-             "&carNumber="+carNumber+
-             "&driverName="+driverName+
-             "&departure="+departure+
-             "&arrival="+arrival+
-             "&distance="+distance+
-             "&departure="+departure)
- return false;
-});
+function dateCarNumberChanged(date, carNumber) {
+    $.get("/starkkrumm/month", { "date": date, "carNumber": carNumber },
+     function (roads) {
+        $("#table").html(convertToHtmlTable(roads));
+     });
+}
+
+function convertToHtmlTable(roads) {
+    var table = "";
+    for (var i in roads) {
+        table += "<tr>"
+               + "<td>" + roads[i].departure + "</td>"
+               + "<td>" + roads[i].arrival + "</td>"
+               + "<td>" + roads[i].roadNumber + "</td>"
+               + "<td>" + roads[i].driverName + "</td>"
+               + "<td>" + roads[i].distance + "</td>"
+               + "<td>" + roads[i].consumption + "</td>"
+               + "</tr>";
+    }
+    return table;
+}
+
+function uploadToDrive(date, carNumber) {
+    $.get("/starkkrumm/print", { "date": date, "carNumber": carNumber });
+}
