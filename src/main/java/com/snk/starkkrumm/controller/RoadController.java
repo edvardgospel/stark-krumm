8e55c0bedf8a491ba1d7d10a63579f9a113e2058
Edvard Adam Eros <edvard_adam_eros@epam.com>
2019-05-18 14:39:03 +0200

    Added logs to endpoints

--- src/main/java/com/snk/starkkrumm/controller/RoadController.java
+++ src/main/java/com/snk/starkkrumm/controller/RoadController.java
@@ -1,21 +1,5 @@
 package com.snk.starkkrumm.controller;
 
-import static com.snk.starkkrumm.service.RoadValidatorService.REQUEST_NULL_MESSAGE;
-import static com.snk.starkkrumm.service.transformer.RoadTransformer.transform;
-import static org.apache.logging.log4j.util.Strings.isBlank;
-
-import java.util.Collections;
-import java.util.List;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.snk.starkkrumm.exception.InvalidRoadException;
 import com.snk.starkkrumm.model.Road;
 import com.snk.starkkrumm.model.RoadRequest;
@@ -23,6 +7,16 @@ import com.snk.starkkrumm.service.RoadService;
 import com.snk.starkkrumm.service.RoadValidatorService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
+
+import static com.snk.starkkrumm.service.RoadValidatorService.REQUEST_NULL_MESSAGE;
+import static com.snk.starkkrumm.service.transformer.RoadTransformer.transform;
+import static org.apache.logging.log4j.util.Strings.isBlank;
 
 @Slf4j
 @Controller
@@ -40,7 +34,7 @@ public class RoadController {
     @ResponseBody
     @PostMapping("/road")
     public void saveRoad(@RequestBody RoadRequest roadRequest, BindingResult bindingResult) {
-        log.info("roadRequest: {}" + roadRequest);
+        log.info("saveRoad endpoint called with RoadRequest: {}", roadRequest);
         if (bindingResult.hasErrors()) {
             throw new InvalidRoadException(REQUEST_NULL_MESSAGE);
         }
@@ -51,6 +45,7 @@ public class RoadController {
     @ResponseBody
     @GetMapping("/road")
     public List<Road> getRoads(@RequestParam("date") String date, @RequestParam("carNumber") Integer carNumber) {
+        log.info("getRoad endpoint called with date: {}, carNumber: {}", date, carNumber);
         validatorService.validate(date, carNumber);
         if (isBlank(date)) {
             return Collections.emptyList();
@@ -63,6 +58,7 @@ public class RoadController {
     public List<Road> deleteRoad(@RequestParam("date") String date,
                                  @RequestParam("carNumber") Integer carNumber,
                                  @RequestParam("roadNumber") Integer roadNumber) {
+        log.info("deleteRoad endpoint called with date: {}, carNumber: {}, roadNumber: {}", date, carNumber, roadNumber);
         validatorService.validate(date, carNumber, roadNumber);
         return roadService.deleteRoad(date, carNumber, roadNumber);
     }
@@ -70,6 +66,7 @@ public class RoadController {
     @ResponseBody
     @PostMapping("/drive/road")
     public void uploadRoad(@RequestParam("date") String date, @RequestParam("carNumber") Integer carNumber) {
+        log.info("uploadRoad endpoint called with date: {}, carNumber: {}", date, carNumber);
         validatorService.validate(date, carNumber);
         roadService.uploadRoad(date, carNumber);
     }
