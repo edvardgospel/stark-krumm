c9ed37190bce460af43da0f0e39c7ac31d250110
Edvard Adam Eros <edvard_adam_eros@epam.com>
2019-05-18 13:55:43 +0200

    Removed unnecessary bean creations

--- src/main/java/com/snk/starkkrumm/config/AppConfig.java
+++ src/main/java/com/snk/starkkrumm/config/AppConfig.java
@@ -1,18 +1,12 @@
 package com.snk.starkkrumm.config;
 
-import javax.sql.DataSource;
-
+import com.snk.starkkrumm.service.ExcelCreationService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.google.api.services.drive.Drive;
-import com.snk.starkkrumm.repository.RoadRepository;
-import com.snk.starkkrumm.service.ExcelCreationService;
-import com.snk.starkkrumm.service.GoogleDriveRequestSenderService;
-import com.snk.starkkrumm.service.RoadService;
-import com.snk.starkkrumm.service.RoadValidatorService;
+import javax.sql.DataSource;
 
 @Configuration
 public class AppConfig {
@@ -39,24 +33,5 @@ public class AppConfig {
         return new ExcelCreationService(inputXls, outputPath);
     }
 
-    @Bean
-    public GoogleDriveRequestSenderService googleDriveRequestSenderService(Drive drive) {
-        return new GoogleDriveRequestSenderService(drive);
-    }
 
-    @Bean
-    public RoadService roadService(ExcelCreationService excelService, RoadRepository roadRepository,
-                                   GoogleDriveRequestSenderService googleDriveRequestSenderService) {
-        return new RoadService(excelService, roadRepository, googleDriveRequestSenderService);
-    }
-
-    @Bean
-    public RoadValidatorService roadValidatorService() {
-        return new RoadValidatorService();
-    }
-
-    @Bean
-    public RoadRepository roadRepository(DataSource dataSource) {
-        return new RoadRepository(dataSource);
-    }
 }
